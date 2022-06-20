69734c8fbc0b420425e96df24525821f0245be8b
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-06-20 17:16:52 +0200

    Add dummy pages

---
+++ pages/nyheter.js
@@ -0,0 +1,109 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+/* This example requires Tailwind CSS v2.0+ */
+import {
+  CheckCircleIcon,
+  ChevronRightIcon,
+  MailIcon,
+} from '@heroicons/react/solid';
+
+const applications = [
+  {
+    applicant: {
+      name: 'Ricardo Cooper',
+      email: 'ricardo.cooper@example.com',
+      imageUrl:
+        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    date: '2020-01-07',
+    dateFull: 'January 7, 2020',
+    stage: 'Completed phone screening',
+    href: '#',
+  },
+  {
+    applicant: {
+      name: 'Kristen Ramos',
+      email: 'kristen.ramos@example.com',
+      imageUrl:
+        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    date: '2020-01-07',
+    dateFull: 'January 7, 2020',
+    stage: 'Completed phone screening',
+    href: '#',
+  },
+  {
+    applicant: {
+      name: 'Ted Fox',
+      email: 'ted.fox@example.com',
+      imageUrl:
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    date: '2020-01-07',
+    dateFull: 'January 7, 2020',
+    stage: 'Completed phone screening',
+    href: '#',
+  },
+];
+
+export default function Example() {
+  return (
+    <div className="overflow-hidden bg-white shadow sm:rounded-md">
+      <ul role="list" className="divide-y divide-gray-200">
+        {applications.map((application) => (
+          <li key={application.applicant.email}>
+            <a href={application.href} className="block hover:bg-gray-50">
+              <div className="flex items-center px-4 py-4 sm:px-6">
+                <div className="flex items-center flex-1 min-w-0">
+                  <div className="flex-shrink-0">
+                    {/* <img className="w-12 h-12 rounded-full" src={application.applicant.imageUrl} alt="" /> */}
+                  </div>
+                  <div className="flex-1 min-w-0 px-4 md:grid md:grid-cols-2 md:gap-4">
+                    <div>
+                      <p className="text-sm font-medium text-indigo-600 truncate">
+                        {application.applicant.name}
+                      </p>
+                      <p className="flex items-center mt-2 text-sm text-gray-500">
+                        <MailIcon
+                          className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                        <span className="truncate">
+                          {application.applicant.email}
+                        </span>
+                      </p>
+                    </div>
+                    <div className="hidden md:block">
+                      <div>
+                        <p className="text-sm text-gray-900">
+                          Applied on{' '}
+                          <time dateTime={application.date}>
+                            {application.dateFull}
+                          </time>
+                        </p>
+                        <p className="flex items-center mt-2 text-sm text-gray-500">
+                          <CheckCircleIcon
+                            className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400"
+                            aria-hidden="true"
+                          />
+                          {application.stage}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <ChevronRightIcon
+                    className="w-5 h-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </div>
+              </div>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
